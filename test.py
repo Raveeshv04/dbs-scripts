cfd4b69c0c281d20ba788972ffdeaa34f57a79ec
Harsh P V (hpv) <hpv@cisco.com>
2022-04-24 01:11:31 +0530

    First commit

---
+++ test.py
@@ -0,0 +1,20 @@
+import os 
+
+class sample:
+    def __init__(self, uname, passw):
+        self.uname = uname
+        self.passw = passw
+        self.__token = ""
+        self.__get_token()
+
+    def __get_token(self):
+        self.__token = self.uname + self.passw
+        return self.uname + self.passw
+
+    
+if __name__ == '__main__':
+    samp = sample("harsh" , "123")
+    print(samp.__token)
+    #print(samp.get_token())
+    
+    
---
+++ wsa_cap.py
@@ -0,0 +1,86 @@
+import requests, urllib3
+import base64, sys
+import json, yaml
+
+
+urllib3.disable_warnings() #suppress output about insecure requests
+
+class wsa:
+    def __init__(self, username, password , wsa_base_url , file_save_location):
+        self.username = username
+        self.password = password
+        self.file_save_location = file_save_location
+        self.policy_endpoint = "/wsa/api/v3.0/web_security/access_policies"
+        self.config_files_endpoint = "wsa/api/v3.0/system_admin/configuration_file"
+        self.wsa_base_url = wsa_base_url
+        self.headers = {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        }
+        self.__token = ""
+        self.__get_token()
+        self.headers["jwtToken"] = self.__token
+
+
+    def __getToken(self):
+        self.username = base64.b64encode(self.username.encode("utf-8"))
+        self.password = base64.b64encode(self.password.encode("utf-8"))
+
+        login_endpoint = "/wsa/api/v2.0/login"
+        body = {
+            "data":
+            {
+                "userName": str(self.username, "utf-8"),
+                "passphrase": str(self.password, "utf-8")
+            }
+        }
+        resp = requests.post(self.wsa_base_url + login_endpoint, headers=self.headers, json=body, verify=False)
+        resp_json = json.loads(resp.text)
+        self.__token = resp_json["data"]["jwtToken"]
+    
+
+    def add_policy(self,access_policies):
+        try:
+            resp = requests.post(self.wsa_base_url + self.policy_endpoint, headers=self.headers, json=access_policies, verify=False)
+
+        except requests.exceptions.RequestException as e:
+            raise SystemExit(e)
+
+    def retrive_policy(self):
+        try:
+            resp = requests.get(self.wsa_base_url + self.policy_endpoint, headers=self.headers, verify=False)
+            resp_json = json.loads(resp.text)
+            print(resp_json)
+
+        except requests.exceptions.RequestException as e:
+            raise SystemExit(e)
+
+    def update_policy(self, policy):
+        try:
+            resp = requests.put(self.wsa_base_url + self.policy_endpoint, headers=self.headers, json=access_policies, verify=False)
+
+        except requests.exceptions.RequestException as e:
+            raise SystemExit(e)
+
+    def get_config_file(self):
+        try:
+            resp = requests.get(self.wsa_base_url + self.config_files_endpoint, headers=self.headers, verify=False)
+            with open("template.csv", 'wb') as csr:
+                for chunk in r.iter_content(chunk_size=1024):
+                    if chunk:
+                        csr.write(chunk) #Recheck this again
+
+        except requests.exceptions.RequestException as e:
+            raise SystemExit(e)
+
+    def create_config_files(self, config_file):
+        try:
+            resp = requests.put(self.wsa_base_url + self.config_files_endpoint, headers=self.headers, json = config_file, verify=False)
+
+        except requests.exceptions.RequestException as e:
+            raise SystemExit(e)
+
+ 
+
+if __name__ == '__main__':
+    wsa_obj = wsa("admin" , "Cisco123" , "http://abc.com" , "./")
